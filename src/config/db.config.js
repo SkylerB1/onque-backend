365a45b9e43f814942518bf4ee3a22d6df9ee014
SkylerB1 <108006513+SkylerB1@users.noreply.github.com>
2023-07-28 13:16:52 +0530

    Delete db.config.js

--- src/config/db.config.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const db = new Sequelize("jjmedia_fe", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false
-});
-
-module.exports = db;
