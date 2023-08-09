84b340048a2f45cf02066e720ca7a133289fa235
aston7332 <aston.k@appwrk.com>
2023-08-09 16:53:14 +0530

    tweet API with media

--- src/routes/fileUpload.js
+++ src/routes/fileUpload.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controller/file.controller");
-const uploadSingleFileMiddleware = require('../middleware/upload');
+const uploadFileMiddleware = require('../middleware/upload');
 
-router.post("/upload", uploadSingleFileMiddleware, controller.upload);
+router.post("/upload", uploadFileMiddleware, controller.upload);
 router.get("/get-files", controller.getListFiles);
 router.get("/get-files/:name", controller.download);
 
