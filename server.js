07d7e999b0368e3b1fcce77946839e493201d10d
aston7332 <aston.k@appwrk.com>
2023-09-05 11:22:36 +0530

    Integrating the My google Business API

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const router = require("./src/routes/index");
-const privet_key = require("./src/config/privkey");
-const https = require("https");
-const fs = require("fs");
-
-
-const https_options = privet_key
+const router =  require("./src/routes/index");
+const path = require('path');
 
 require("dotenv").config();
 require("./src/config/db.config");
@@ -16,8 +11,8 @@ require("./src/models/index");
 
 global._basedir = __dirname;
 
-const corsOptions = {
-  origin: "*",
+var corsOptions = {
+  origin: "*"
 };
 
 app.use(cors(corsOptions));
@@ -26,7 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // global-route
 app.use("/api", router);
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-const httpsServer = https.createServer(https_options, app);
-
-httpsServer.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port: ${process.env.PORT}`);
+  });
---
+++ src/controller/googleBusinessController.js
@@ -0,0 +1,67 @@
+const UserService = require("../services/userServices");
+const { google } = require("googleapis");
+const userInterface = new UserService();
+const moment = require("moment");
+const { OAuth2Client } = require("google-auth-library");
+const axios = require("axios");
+
+const client = new OAuth2Client({
+  clientId:
+    "585227327062-na3cm988g4a3lkstuepo9snk7e0ra4cm.apps.googleusercontent.com",
+  clientSecret: "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",
+  redirectUri: "http://localhost:3000/api/auth/callback/google",
+});
+
+const getGoogleBusinessAuthUrl = async (req, res) => {
+  try {
+    const accessToken = req.headers.accesstoken;
+    const platform = req.headers.platform;
+    // console.log(accessToken, platform)
+    // return
+
+    const response = await axios.get(
+      "https://mybusinessaccountmanagement.googleapis.com/v1/accounts",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    res.json(response.data);
+    const accountsname = response.data.accounts[0].accountName;
+
+    const userData = {
+      accessToken: accessToken,
+      accessSecret: "",
+      platform: platform,
+      screenName: response.data.accounts[0].name,
+    };
+    console.log(userData);
+    await userInterface.setMediaToken(userData);
+  } catch (error) {
+    console.error("Error fetching business info:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+  //   console.log(req.body);
+};
+
+const googleBusinessPost = async (req, res) => {
+  try{ const headers = JSON.parse(req.headers.headersarraystring);
+    const getHeaders = (platform) => {
+      return headers.filter((headers) => headers.platform === platform)[0];
+    };
+  
+    const googleBusinessHeaders = getHeaders("google-business");
+    console.log(googleBusinessHeaders)
+    const accessToken = googleBusinessHeaders.accessToken;
+    const getMediaType = req.body.getMediaType;
+    console.log(accessToken, getMediaType)
+
+
+  }catch(err){
+
+  }
+  // console.log(req, req.body);
+};
+
+module.exports = { getGoogleBusinessAuthUrl, googleBusinessPost };
