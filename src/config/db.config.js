a7bdacb29dae2a602524e41ae67b370c8a7808fe
Skyler B <skyler.b@appwrk.com>
2024-03-14 12:37:18 +0530

    integrate env variable in db config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,18 +1,10 @@
 const Sequelize = require("sequelize");
-const environment = require("../config/db.config.json");
+const { DB_NAME, DB_USERNAME, DB_PASS, DIALECT, DB_HOST } = process.env;
 
-
-DB_NAME = environment.development.database;
-USER_NAME = environment.development.username;
-PASSWORD = environment.development.password;
-HOST = environment.development.host;
-DIALECT = environment.development.dialect;
-LOGGING = environment.development.logging;
-
-const db = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
-  host: HOST,
+const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASS, {
+  host: DB_HOST,
   dialect: DIALECT,
-  logging: LOGGING
+  logging: false,
 });
 
 module.exports = db;
